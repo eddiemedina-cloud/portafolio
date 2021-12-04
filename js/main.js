0567087e4d8e3558ee2805ee3035dbf2e2cf49fa
Eddie Medina <eddiemedina110@gmail.com>
2021-12-03 21:18:52 -0600

    actualizando portafolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('load', () => {
+    document.getElementById("loader").classList.toggle("loader2")
+})
+
+ScrollReveal().reveal('.animacion', { delay: 500 });
+ScrollReveal().reveal('.about-animation', { delay: 500 });
+ScrollReveal().reveal('.about-container-grid', { delay: 500 });
